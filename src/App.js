dde1ab58c88cf3e001d7399ce7521565efc36e88
Pham Huy <gotit@gotits-MacBook-Pro.local>
2019-01-01 01:55:53 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+// import AddItemForm from './components/AddItemForm';
+// import Login from './components/Login';
+// import Item from './components/Item';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container main-panel">
+        <Home />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
