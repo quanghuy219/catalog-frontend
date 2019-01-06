d6672147241eeadf4fb4730dea510f35297afc7f
Pham Huy <p.q.huy97@gamil.com>
2019-01-06 21:52:28 +0700

    Updates

---
+++ precache-manifest.353788983a89deec102385e58f42e7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44c56d18f6b1d5a45553",
+    "url": "/catalog-frontend/static/js/runtime~main.44c56d18.js"
+  },
+  {
+    "revision": "1e3c1581570ed4ba1ef9",
+    "url": "/catalog-frontend/static/js/main.1e3c1581.chunk.js"
+  },
+  {
+    "revision": "92dffe7aa44985b1f3f2",
+    "url": "/catalog-frontend/static/js/1.92dffe7a.chunk.js"
+  },
+  {
+    "revision": "1e3c1581570ed4ba1ef9",
+    "url": "/catalog-frontend/static/css/main.02e27313.chunk.css"
+  },
+  {
+    "revision": "92dffe7aa44985b1f3f2",
+    "url": "/catalog-frontend/static/css/1.4f4336d0.chunk.css"
+  },
+  {
+    "revision": "cc70273a0f87f57ed46c6b3eb87983b6",
+    "url": "/catalog-frontend/index.html"
+  }
+];
