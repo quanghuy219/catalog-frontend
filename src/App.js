6488bd23ec4276dda4c56e32010ebf58da8be320
Pham Huy <p.q.huy97@gamil.com>
2019-01-06 23:00:07 +0700

    base name for github page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/catalog-frontend">
       <div className="App">
         <Navbar />
         <div className="container main-panel">
