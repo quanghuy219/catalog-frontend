8e2a856904a4a4471c1ab51304634e8ab8b142e9
Pham Huy <p.q.huy97@gamil.com>
2019-01-21 11:53:55 +0700

    Remove unused service worker

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import ReduxPromise from 'redux-promise';
 import thunk from 'redux-thunk';
 import App from './App';
 import reducers from './reducers';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 
@@ -28,8 +27,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
