dffd3633503f8037ba83a3542bfa3bf4f4a221b0
Pham Huy <p.q.huy97@gamil.com>
2019-01-24 12:36:54 +0700

    Set config values based on NODE_ENV

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,6 +6,6 @@ const prod = {
   API_URL: 'https://catalog-backend.herokuapp.com',
 };
 
-const CONFIG = (process.env.ENV === 'production') ? prod : dev;
+const CONFIG = (process.env.NODE_ENV === 'production') ? prod : dev;
 
 export default CONFIG;
